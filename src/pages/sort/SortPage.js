9703e7501a49d1fcba2f538bb12ca8bd4c800484
Santiago Garcia <santiagogarcia97@outlook.com>
2020-06-05 23:48:32 -0300

    bubble sort working

--- src/pages/sort/SortPage.js
+++ src/pages/sort/SortPage.js
@@ -3,7 +3,6 @@ import ArrayItem from './ArrayItem'
 
 const SortPage = () => {
   const [array, setArray] = useState([])
-  console.log(array)
 
   const generateArray = () => {
     const newArray = []
@@ -12,6 +11,31 @@ const SortPage = () => {
     }
     setArray(newArray)
   }
+
+  function task(auxArr, i) {
+    setTimeout(function () {
+      setArray(auxArr)
+    }, 25 * i)
+  }
+
+  const bubbleSort = () => {
+    console.log('sort', array)
+    const n = array.length
+    const auxArr = [...array]
+    let count = 1
+    for (let i = 0; i < n; i += 1) {
+      for (let j = 0; j < n; j += 1) {
+        if (auxArr[j] > auxArr[j + 1]) {
+          const aux = auxArr[j]
+          auxArr[j] = auxArr[j + 1]
+          auxArr[j + 1] = aux
+          task([...auxArr], count)
+          count += 1
+        }
+      }
+    }
+  }
+
   return (
     <div>
       Sort page
@@ -19,8 +43,15 @@ const SortPage = () => {
         <button type="button" onClick={generateArray}>
           generate array
         </button>
-        <button type="button">bubble</button>
+        <button type="button" onClick={bubbleSort}>
+          bubble
+        </button>
         <button type="button">merge</button>
+      </div>
+      <div>
+        {array.map((e) => {
+          return ` ${e} `
+        })}
       </div>
       <div className="flex-container">
         {array.map((number, i) => (
