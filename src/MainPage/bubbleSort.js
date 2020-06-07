2db14ef75562c1baf1d6a74562f02e0ee7a61c65
Santiago Garcia <santiagogarcia97@outlook.com>
2020-06-07 02:08:27 -0300

    added animation to bubble sort

--- src/MainPage/bubbleSort.js
+++ src/MainPage/bubbleSort.js
@@ -1,25 +1,52 @@
-const iteration = (auxArr, i, setArray) => {
-  setTimeout(() => {
-    setArray(auxArr)
-  }, 25 * i)
-}
+const bubleSort = (array) => {
+  // Wrapper function for animation
+  let animationCount = 1
+  const delay = (callback) => {
+    setTimeout(() => {
+      callback()
+    }, 10 * animationCount)
+    animationCount += 1
+  }
 
-const bubleSort = (array, setArray) => {
   // eslint-disable-next-line react/prop-types
   const n = array.length
   const auxArr = [...array]
-  let count = 1
+  // Array of html div elements
+  const bars = document.getElementsByClassName('bar')
 
   for (let i = 0; i < n; i += 1) {
-    for (let j = 0; j < n; j += 1) {
+    for (let j = 0; j < n - i - 1; j += 1) {
+      // Set colors of elements being compared
+      delay(() => {
+        bars[j].style.backgroundColor = 'darkcyan'
+        bars[j + 1].style.backgroundColor = 'darkblue'
+      })
+
       if (auxArr[j] > auxArr[j + 1]) {
         const aux = auxArr[j]
         auxArr[j] = auxArr[j + 1]
         auxArr[j + 1] = aux
-        iteration([...auxArr], count, setArray)
-        count += 1
+
+        // Swap divs properties
+        delay(() => {
+          const auxHeight = bars[j].style.height
+          bars[j].style.height = bars[j + 1].style.height
+          bars[j].style.backgroundColor = 'darkblue'
+          bars[j + 1].style.height = auxHeight
+          bars[j + 1].style.backgroundColor = 'darkcyan'
+        })
       }
+
+      // Set colors back to original
+      delay(() => {
+        bars[j].style.backgroundColor = 'darkred'
+        bars[j + 1].style.backgroundColor = 'darkred'
+      })
     }
+    delay(() => {
+      bars[n - i - 1].style.backgroundColor = 'darkgreen'
+    })
   }
 }
+
 export default bubleSort
