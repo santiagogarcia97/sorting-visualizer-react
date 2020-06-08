88cf0b59ae701ba8c66df8f3875dcbddf5393c2a
Santiago Garcia <santiagogarcia97@outlook.com>
2020-06-08 12:20:38 -0300

    menu css changes

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react'
 import Menu from './Menu'
 
+// Gets random number from interval. From mozilla examples
+const getRandomArbitrary = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
 const MainPage = () => {
   const [array, setArray] = useState([])
 
+  const generateArray = (arrayLength) => {
+    const newArray = []
+    for (let i = 0; i < parseInt(arrayLength, 10); i += 1) {
+      newArray[i] = {
+        value: getRandomArbitrary(10, 1000),
+        color: 'darkred',
+      }
+    }
+    setArray(newArray)
+  }
+
+  if (array.length === 0) generateArray(10)
   return (
     <div className="container main">
-      <Menu array={array} setArray={setArray} />
+      <Menu array={array} setArray={setArray} generateArray={generateArray} />
+
       <div className="flex-container">
         {array.map((element, i) => (
           <div
@@ -18,6 +36,8 @@ const MainPage = () => {
           />
         ))}
       </div>
+
+      <div className="bottom-bar" />
     </div>
   )
 }
--- src/MainPage/Menu.js
+++ src/MainPage/Menu.js
@@ -1,26 +1,9 @@
 import React, { useState } from 'react'
 import bubleSort from './bubbleSort'
 
-// Gets random number from interval. From mozilla examples
-const getRandomArbitrary = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min)
-}
-
-// eslint-disable-next-line react/prop-types
-const Menu = ({ array, setArray }) => {
-  const [arrayLenght, setArrayLenght] = useState(10)
-  const [delay, setDelay] = useState(100)
-
-  const generateArray = () => {
-    const newArray = []
-    for (let i = 0; i < arrayLenght; i += 1) {
-      newArray[i] = {
-        value: getRandomArbitrary(10, 1000),
-        color: 'darkred',
-      }
-    }
-    setArray(newArray)
-  }
+const Menu = ({ array, setArray, generateArray }) => {
+  const [arrayLength, setArrayLength] = useState('10')
+  const [delay, setDelay] = useState('100')
 
   const handleSortClick = () => {
     bubleSort(array, setArray, delay)
@@ -28,64 +11,69 @@ const Menu = ({ array, setArray }) => {
 
   return (
     <div>
-      <div className="options">
-        <div className="form-row">
-          <div className="form-group">
-            <label htmlFor="arrayLenght">Number of elements</label>
-            <input
-              id="arrayLenght"
-              min="5"
-              max="300"
-              type="number"
-              onChange={(event) =>
-                setArrayLenght(parseInt(event.target.value, 10))
-              }
-              value={arrayLenght}
-            />
-            <small>Min: 5 - Max: 300</small>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="delay">Delay per iteration (ms)</label>
-            <input
-              id="delay"
-              min="1"
-              max="2000"
-              type="number"
-              onChange={(event) => setDelay(parseInt(event.target.value, 10))}
-              value={delay}
-            />
-            <small>Min: 1 - Max: 2000</small>
-          </div>
+      <div className="form-row">
+        <div className="form-group">
+          <label htmlFor="arrayLenght">Array lenght: {arrayLength}</label>
+          <input
+            id="arrayLenght"
+            type="range"
+            onChange={(event) => setArrayLength(event.target.value)}
+            value={arrayLength}
+            min="5"
+            max="100"
+          />
+          <small>Min: 5 - Max: 100</small>
         </div>
 
-        <div className="form-row">
-          <div className="form-group">
-            <label htmlFor="algorithm">Algorithm</label>
-            <select id="algorithm" name="algorithm">
-              <option value="bubble">Bubble</option>
-              <option value="bubble">Merge</option>
-              <option value="bubble">Heap</option>
-            </select>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="render">Render as</label>
-            <select id="render" name="render">
-              <option value="bubble">Bars</option>
-              <option value="bubble">Dots</option>
-            </select>
-          </div>
+        <div className="form-group">
+          <label htmlFor="delay">Iteration delay: {delay}ms</label>
+          <input
+            id="delay"
+            type="range"
+            onChange={(event) => setDelay(event.target.value)}
+            value={delay}
+            min="1"
+            max="1000"
+          />
+          <small>Min: 1 - Max: 1000</small>
         </div>
       </div>
 
-      <div className="menu">
-        <button type="button" onClick={generateArray}>
-          Generate array
-        </button>
-        <button type="button" onClick={handleSortClick}>
-          Sort
-        </button>
+      <div className="form-row">
+        <div className="form-group">
+          <label htmlFor="algorithm">Algorithm</label>
+          <select id="algorithm" name="algorithm">
+            <option value="bubble">Bubble</option>
+            <option value="bubble">Merge</option>
+            <option value="bubble">Heap</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="render">Render as</label>
+          <select id="render" name="render">
+            <option value="bubble">Bars</option>
+            <option value="bubble">Dots</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="form-row">
+        <div className="form-group">
+          <button
+            type="button"
+            onClick={() => {
+              generateArray(arrayLength)
+            }}
+          >
+            Generate array
+          </button>
+        </div>
+        <div className="form-group">
+          <button type="button" onClick={handleSortClick}>
+            Sort
+          </button>
+        </div>
       </div>
     </div>
   )
